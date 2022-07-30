5522004e2c0a6a82f7a8336edc5ee39a4fa9c975
NishanAhirao <ahiraonishant@gmail.com>
2022-07-30 18:58:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextArea from "./components/TextArea";
+ 
+
+function App() {
+  return (
+    <>
+      <Navbar  title="TextConverter" />
+      <div id="mycont" className="container my-4 ">
+        <TextArea title="You can enter text below" />
+       </div>
+       <footer>
+        <div class="center">
+            Copyright &copy; www.myOnlineMeal.com. All rights reserved!
+        </div>
+    </footer>
+    </>
+  );
+}
+
+export default App;
